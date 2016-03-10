d8273c27424a4441335baca9b78d763f79dcb6b9
Kevin Nelson <kpie314@gmail.com>
2016-03-10 13:24:57 -0800

    fallback if webgl is not supported

--- PrettyGraphics.js
+++ PrettyGraphics.js
@@ -1,7 +1,8 @@
 let scene = new THREE.Scene();
 let camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
 
-let renderer = new THREE.WebGLRenderer({ alpha: true });
+let renderer = Detector.webgl? new THREE.WebGLRenderer({ alpha: true }): new THREE.CanvasRenderer({ alpha: true });
+//new THREE.WebGLRenderer({ alpha: true });
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
