2e268b3786f2039049a6716ce91d544e83de5515
Kevin Nelson <kpie314@gmail.com>
2016-03-10 14:41:55 -0800

    get Detector for webGl fallback

--- PrettyGraphics.js
+++ PrettyGraphics.js
@@ -2,30 +2,16 @@ let scene = new THREE.Scene();
 let camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
 
 // http://stackoverflow.com/questions/9899807/three-js-detect-webgl-support-and-fallback-to-regular-canvas
-function webglAvailable() {
-    try {
-        var canvas = document.createElement("canvas");
-        return !!
-            window.WebGLRenderingContext && 
-            (canvas.getContext("webgl") || 
-			canvas.getContext("experimental-webgl"));
-    } catch(e) { 
-        return false;
-    } 
-}
 
-let renderer = webglAvailable()? new THREE.WebGLRenderer({ alpha: true }): new THREE.CanvasRenderer({ alpha: true });
-//new THREE.WebGLRenderer({ alpha: true });
+
+let renderer =  Detector.webgl ? new THREE.WebGLRenderer({ alpha: true }): new THREE.CanvasRenderer({ alpha: true });
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
+// http://threejs.org/docs/#Manual/Introduction/Creating_a_scene
+
 let geometry = new THREE.BoxGeometry( 2.5, 2.5, 2.5 );
-let material = new THREE.MeshLambertMaterial( {
-      color: 0xFFFFFF,
-      shading: THREE.SmoothShading,
-      ambient: 0x555555,
-      specular: 0xffffff
-    } );
+let material = new THREE.MeshPhongMaterial( { color: 0xdddddd, specular: 0x009900, shininess: 70, shading: THREE.FlatShading } )
 let cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 
@@ -44,6 +30,7 @@ let render = function () {
 
 	cube.rotation.x += 0.01;
 	cube.rotation.y += 0.01;
+	cube.rotation.z += 0.005;
 
 	renderer.render(scene, camera);
 };
