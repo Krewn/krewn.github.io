f85f8d61ff32e53f09c46e3f82df66d08871e090
Kevin Nelson <kpie314@gmail.com>
2016-03-10 13:37:07 -0800

    fix webgl roll back.

--- PrettyGraphics.js
+++ PrettyGraphics.js
@@ -1,7 +1,15 @@
 let scene = new THREE.Scene();
 let camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
 
-let renderer = Detector.webgl? new THREE.WebGLRenderer({ alpha: true }): new THREE.CanvasRenderer({ alpha: true });
+let CheckWebgl = function () {
+	try {
+		var canvas = document.createElement( 'canvas' );
+		return !! ( window.WebGLRenderingContext && ( canvas.getContext( 'webgl' ) || canvas.getContext( 'experimental-webgl' ) ) );
+	} catch ( e ) {
+		return false;
+}}; 
+
+let renderer = CheckWebgl()? new THREE.WebGLRenderer({ alpha: true }): new THREE.CanvasRenderer({ alpha: true });
 //new THREE.WebGLRenderer({ alpha: true });
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
