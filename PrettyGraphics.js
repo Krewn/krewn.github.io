e254b723d995d70ca72b15e419765d04614f003f
Kevin Nelson <kpie314@gmail.com>
2016-03-10 19:48:09 -0800

    refactor

--- PrettyGraphics.js
+++ PrettyGraphics.js
@@ -1,38 +1,39 @@
 
 if ( ! Detector.webgl ) Detector.addGetWebGLMessage();
 
+let aspect = window.innerWidth/window.innerHeight;
+
+let scene , camera , renderer , geometry , material , cube , directionalLight , backLight;
 function init(){
-	let scene = new THREE.Scene();
-	let camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+	scene = new THREE.Scene();
+	camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
 
 	// http://stackoverflow.com/questions/9899807/three-js-detect-webgl-support-and-fallback-to-regular-canvas
 
 
-	let renderer =  Detector.webgl ? new THREE.WebGLRenderer({ alpha: true }): new THREE.CanvasRenderer({ alpha: true });
+	renderer =  Detector.webgl ? new THREE.WebGLRenderer({ alpha: true }): new THREE.CanvasRenderer({ alpha: true });
 	renderer.setSize( window.innerWidth, window.innerHeight );
 	document.body.appendChild( renderer.domElement );
 
 	// http://threejs.org/docs/#Manual/Introduction/Creating_a_scene
 
-	let geometry = new THREE.BoxGeometry( 2.5, 2.5, 2.5 );
-	let material = new THREE.MeshPhongMaterial( { color: 0xdddddd, specular: 0x009900, shininess: 70, shading: THREE.FlatShading } )
-	let cube = new THREE.Mesh( geometry, material );
+	geometry = new THREE.BoxGeometry( 2.5, 2.5, 2.5 );
+	material = new THREE.MeshPhongMaterial( { color: 0xdddddd, specular: 0x009900, shininess: 70, shading: THREE.FlatShading } )
+	cube = new THREE.Mesh( geometry, material );
 	scene.add( cube );
 
-	let directionalLight = new THREE.DirectionalLight( 0xffffff, 0.5 );
+	directionalLight = new THREE.DirectionalLight( 0xffffff, 0.5 );
 	directionalLight.position.set( 0, 1, 0 );
+	
+	backLight = new THREE.DirectionalLight( 0x444444, 0.5 );
+	backLight.position.set( 1, -1, 0.5 );
+	
 	scene.add( directionalLight );
-	let directionalLight2 = new THREE.DirectionalLight( 0x444444, 0.5 );
-	directionalLight2.position.set( 1, -1, 0.5 );
-	scene.add( directionalLight );
-	scene.add( directionalLight2 );
+	scene.add( backLight );
 
 	camera.position.z = 5;
-	
-	window.addEventListener( 'resize', onWindowResize, false );
-
 }
-let onWindowResize = function (){
+function onWindowResize(){
 
 		camera.aspect = window.innerWidth / window.innerHeight;
 		camera.updateProjectionMatrix();
@@ -40,7 +41,7 @@ let onWindowResize = function (){
 		renderer.setSize( window.innerWidth, window.innerHeight );
 
 }
-let render = function () {
+function render() {
 		requestAnimationFrame( render );
 
 		cube.rotation.x += 0.01;
@@ -49,4 +50,6 @@ let render = function () {
 
 		renderer.render(scene, camera);
 };
+init();
+window.addEventListener( 'resize', onWindowResize, false );
 render();
